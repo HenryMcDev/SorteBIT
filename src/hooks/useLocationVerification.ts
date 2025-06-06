54f18b78b88f1509b70ff681f8a40ea165a89fc5
HenryMcDev <henryoficial37@gmail.com>
2025-06-06 09:18:10 -0300

    Update useLocationVerification.ts

--- src/hooks/useLocationVerification.ts
+++ src/hooks/useLocationVerification.ts
@@ -12,7 +12,7 @@ const SCHOOL_COORDINATES = {
   latitude: -19.741150,
   longitude: -47.931570
 };
-const ALLOWED_RADIUS_METERS = 100;
+const ALLOWED_RADIUS_METERS = 200;
 
 // Função para calcular a distância entre duas coordenadas usando a fórmula de Haversine
 const calculateDistance = (lat1: number, lon1: number, lat2: number, lon2: number): number => {
